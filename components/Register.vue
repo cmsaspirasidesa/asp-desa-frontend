53683334e5032b9a0634a085269f42cdafe65913
dandnirv7 <dandnirv7@gmail.com>
2023-07-21 15:22:20 +0700

    refactor:stisla to flowbite

--- components/Register.vue
+++ components/Register.vue
@@ -1,46 +1,12 @@
 <template>
-  <div>
-    <section class="section">
-      <div class="container mt-4">
-        <div class="row">
-          <div class="col-12">
-            <div class="row">
-              <div class="col-md-6">
-                <div class="card card-primary">
-                  <div class="card-body">
-                    <FormRegister />
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-6" style="margin-top: 144px">
-                <div class="col">
-                  <div class="greeting">
-                    <h1 class="text-center px-5">
-                      Selamat Datang di Desa Sukamaju
-                    </h1>
-                  </div>
-                  <div class="content pt-3 px-5">
-                    <p class="text-center fs-1">
-                      Disini warga bisa memberikan aspirasi demi kemajuan desa
-                      kita semua, dengan semua aspirasi anda desa ini akan
-                      memberikan pelayanan terbaik bagi semua warga
-                    </p>
-                  </div>
-                  <div
-                    class="login px-5 col d-flex flex-column align-items-center mt-5"
-                  >
-                    <h3 class="text-center">Sudah punya akun?</h3>
-                    <button class="btn btn-outline-primary col-4 mt-3">
-                      Masuk
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <!-- <div class="simple-footer">Copyright &copy; Stisla 2018</div> -->
-          </div>
-        </div>
-      </div>
-    </section>
+  <div class="flex">
+    <Flowbit class="w-3/4" />
+    <RegisterContent class="w-2/4" />
   </div>
 </template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ components/RegisterContent.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="section bg-gray-50 dark:bg-gray-900 pt-40">
+    <div class="flex flex-col text-center bg-red-500">
+      <div class="greeting">
+        <h1 class="text-3xl font-bold">Selamat Datang di Desa Sukamaju</h1>
+      </div>
+      <div class="content mt-4">
+        <p class="text-2xl">
+          Disini warga bisa memberikan aspirasi demi kemajuan desa kita semua,
+          dengan semua aspirasi anda desa ini akan memberikan pelayanan terbaik
+          bagi semua warga
+        </p>
+      </div>
+
+      <div class="login mt-5">
+        <h3 class="text-2xl">Sudah punya akun?</h3>
+        <button
+          class="w-max text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+        >
+          Masuk
+        </button>
+      </div>
+    </div>
+    <!-- <div class="">
+      <div class="flex flex-col">
+        <div class="greeting">
+          <h1 class="text-center px-5">Selamat Datang di Desa Sukamaju</h1>
+        </div>
+        <div class="content pt-3 px-5">
+          <p class="text-center fs-1">
+            Disini warga bisa memberikan aspirasi demi kemajuan desa kita semua,
+            dengan semua aspirasi anda desa ini akan memberikan pelayanan
+            terbaik bagi semua warga
+          </p>
+        </div>
+        <div class="login px-5 col d-flex flex-column align-items-center mt-5">
+          <h3 class="text-center">Sudah punya akun?</h3>
+          <button class="btn btn-outline-primary col-4 mt-3">Masuk</button>
+        </div>
+      </div>
+    </div> -->
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
