dd3f8bf02a4e8d453c9fad66383ba8ba6f7a7834
rhenoresky <rheno2002@gmail.com>
2023-07-22 00:19:41 +0700

    refactor: icon user & logo desa

--- components/NavbarUser.vue
+++ components/NavbarUser.vue
@@ -1,5 +1,7 @@
 <script setup>
 import {Disclosure, Menu, MenuButton, MenuItem, MenuItems} from '@headlessui/vue'
+import {UserCircleIcon} from '@heroicons/vue/24/solid';
+import Desa from '../assets/image/Desa.png'
 </script>
 
 <template>
@@ -7,9 +9,9 @@ import {Disclosure, Menu, MenuButton, MenuItem, MenuItems} from '@headlessui/vue
     <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
       <div class="relative flex h-16 items-center justify-between">
         <div class="flex flex-1 items-center justify-start">
-          <div class="flex flex-shrink-0 items-center">
-            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-              alt="Your Company" />
+          <div class="flex flex-shrink-0 items-center gap-2">
+            <img class="h-8 w-auto" :src="Desa" alt="Your Company" />
+            <h1 class="text-white text-xl font-medium">Desa Sukaratu</h1>
           </div>
         </div>
         <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
@@ -17,9 +19,7 @@ import {Disclosure, Menu, MenuButton, MenuItem, MenuItems} from '@headlessui/vue
             <div>
               <MenuButton
                 class="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                <img class="h-8 w-8 rounded-full"
-                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt="" />
+                <UserCircleIcon class="h-8 w-8 rounded-full text-white" />
               </MenuButton>
             </div>
             <transition enter-active-class="transition ease-out duration-100"
