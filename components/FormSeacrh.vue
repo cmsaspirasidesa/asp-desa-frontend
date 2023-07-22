01bf4961a74136cee05275e297accdf656b8fd8c
rhenoresky <rheno2002@gmail.com>
2023-07-22 16:55:32 +0700

    feat: list-aspirasi-user

--- components/FormSeacrh.vue
+++ components/FormSeacrh.vue
@@ -3,11 +3,13 @@ import {MagnifyingGlassIcon} from '@heroicons/vue/24/solid';
 </script>
 
 <template>
-  <div class="relative mx-auto text-gray-600 w-[300px] mt-10">
-    <input class="border-2 border-gray-300 w-full bg-white h-10 rounded-lg text-sm focus:outline-none" type="search"
-      name="search" placeholder="Search">
-    <button type="submit" class="absolute right-0 -top-2 mt-5 mr-4">
-      <MagnifyingGlassIcon class="text-gray-600 h-4 w-4 fill-current" />
-    </button>
+  <div class="mx-2">
+    <div class="relative m-auto text-gray-600 max-w-[300px] w-full mt-10">
+      <input class="border-2 border-gray-300 w-full bg-white h-10 rounded-lg text-sm focus:outline-none" type="search"
+        name="search" placeholder="Search">
+      <button type="submit" class="absolute right-0 -top-2 mt-5 mr-4">
+        <MagnifyingGlassIcon class="text-gray-600 h-4 w-4 fill-current" />
+      </button>
+    </div>
   </div>
 </template>
--- pages/user.vue
+++ pages/user.vue
@@ -1,7 +1,46 @@
+<script setup>
+import Masjid from '../assets/image/Masjid1.jpg'
+const aspirasi = [
+  {
+    id: 1,
+    judul: 'Bangun Masjid',
+    imageSrc: Masjid,
+    status: 'Proses',
+    deskripsi: 'lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe',
+  },
+  {
+    id: 1,
+    judul: 'Bangun Masjid',
+    imageSrc: Masjid,
+    status: 'Proses',
+    deskripsi: 'lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe',
+  },
+  {
+    id: 1,
+    judul: 'Bangun Masjid',
+    imageSrc: Masjid,
+    status: 'Proses',
+    deskripsi: 'lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe',
+  },
+  {
+    id: 1,
+    judul: 'Bangun Masjid',
+    imageSrc: Masjid,
+    status: 'Proses',
+    deskripsi: 'lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe',
+  },
+]
+</script>
+
 <template>
   <NavbarUser />
-  <div class="flex flex-col md:flex-row">
+  <div class="flex flex-col md:flex-row gap-8 lg:mx-8">
     <FormAspirasi />
-    <FormSeacrh />
+    <div>
+      <FormSeacrh />
+      <div class="flex gap-y-6 mt-6 flex-wrap">
+        <CardAspirasi v-for="item of aspirasi" :aspirasi="item" :key="item.id" />
+      </div>
+    </div>
   </div>
 </template>
