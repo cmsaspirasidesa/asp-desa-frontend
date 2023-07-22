a2bbe8cfbb7c60934571fbfcb8dac0fd402934c1
rhenoresky <rheno2002@gmail.com>
2023-07-22 19:44:00 +0700

    refactor: change atribut

--- components/FormAspirasi.vue
+++ components/FormAspirasi.vue
@@ -6,51 +6,51 @@ import {PhotoIcon} from '@heroicons/vue/24/solid'
   <form
     class="md:max-w-[400px] max-w-[350px] w-full m-auto md:mt-10 flex flex-col px-2 py-5 sm:p-10 rounded-[12px] md:border md:border-solid md:border-gray-300">
     <div>
-      <label for="street-address" class="block text-sm font-medium leading-6 text-gray-900">Nama</label>
+      <label for="nama" class="block text-sm font-medium leading-6 text-gray-900">Nama</label>
       <div class="mt-2">
-        <input type="text" required="true" name="street-address" id="street-address" autocomplete="street-address"
+        <input type="text" required="true" name="nama" id="nama" autocomplete="nama"
           class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
       </div>
     </div>
     <div class="mt-6">
-      <label for="street-address" class="block text-sm font-medium leading-6 text-gray-900">Email</label>
+      <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email</label>
       <div class="mt-2">
-        <input type="email" required="true" name="street-address" id="street-address" autocomplete="street-address"
+        <input type="email" required="true" name="email" id="email" autocomplete="email"
           class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
       </div>
     </div>
     <div class="mt-6">
-      <label for="street-address" required="true" class="block text-sm font-medium leading-6 text-gray-900">Judul</label>
+      <label for="judul" required="true" class="block text-sm font-medium leading-6 text-gray-900">Judul</label>
       <div class="mt-2">
-        <input type="text" name="street-address" id="street-address" autocomplete="street-address"
+        <input type="text" name="judul" id="judul" autocomplete="judul"
           class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
       </div>
     </div>
     <div class="mt-6">
-      <label for="street-address" required="true" class="block text-sm font-medium leading-6 text-gray-900">Alamat</label>
+      <label for="alamat" required="true" class="block text-sm font-medium leading-6 text-gray-900">Alamat</label>
       <div class="mt-2">
-        <input type="text" name="street-address" id="street-address" autocomplete="street-address"
+        <input type="text" name="alamat" id="alamat" autocomplete="alamat"
           class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
       </div>
     </div>
     <div class="mt-6">
-      <label for="about" class="block text-sm font-medium leading-6 text-gray-900">Deskripsi</label>
+      <label for="deskripsi" class="block text-sm font-medium leading-6 text-gray-900">Deskripsi</label>
       <div class="mt-2">
-        <textarea id="about" name="about" rows="5"
+        <textarea id="deskripsi" name="deskripsi" rows="5"
           class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
       </div>
     </div>
 
     <div class="mt-6">
-      <label for="cover-photo" class="block text-sm font-medium leading-6 text-gray-900">Foto</label>
+      <p class="block text-sm font-medium leading-6 text-gray-900">Foto</p>
       <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
         <div class="text-center">
           <PhotoIcon class="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
           <div class="mt-4 flex text-sm leading-6 text-gray-600">
-            <label for="file-upload"
+            <label for="foto"
               class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
               <span>Upload a file</span>
-              <input id="file-upload" name="file-upload" type="file" class="sr-only" />
+              <input id="foto" name="foto" type="file" class="sr-only" />
             </label>
             <p class="pl-1">or drag and drop</p>
           </div>
--- pages/login.vue
+++ pages/login.vue
@@ -36,8 +36,7 @@ function ubah() {
             <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
           </div>
           <div class="mt-2 relative">
-            <input id="password" name="password" :type="tipe" autocomplete="current-password" required="true"
-              v-model="password"
+            <input id="password" name="password" :type="tipe" autocomplete="password" required="true" v-model="password"
               class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
             <a class="absolute right-3 top-[4px] cursor-pointer" @click="ubah">
               <Icon :name="mata" class="text-gray-500 text-[24px]" />
