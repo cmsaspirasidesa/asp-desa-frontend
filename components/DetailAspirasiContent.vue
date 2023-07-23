575048ef39657f44ef25749116ba79a5b587b05a
dandnirv7 <dandnirv7@gmail.com>
2023-07-23 22:44:12 +0700

    refactor: add detail_aspirasi

---
+++ components/DetailAspirasiContent.vue
@@ -0,0 +1,46 @@
+<template>
+  <div
+    class="flex flex-col w-full h-screen px-5 pt-10 mx-auto bg-gray-200 xl:px-24 xl:pt-24 lg:pt-16 lg:px-16 md:py-16 md:px-16 md:mt-0 xl:min-h-screen"
+  >
+    <div class="subject">
+      <h1 class="text-3xl font-bold lg:text-4xl md:text-4xl">
+        Pembangunan Lapangan
+      </h1>
+    </div>
+    <div class="mt-5 description">
+      <h2 class="text-2xl font-bold lg:text-3xl md:text-3xl">Deskripsi</h2>
+      <p class="mt-4 text-md lg:text-lg md:text-lg">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua Ut enim ad minim
+        veniam nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cill eu fugiat nulla pariatur. Excepteur sint occaecat
+        proident, sunt in culpa qui officia deserunt molli.
+      </p>
+    </div>
+    <div class="mt-4 location">
+      <h2 class="text-2xl font-bold lg:text-3xl md:text-3xl">Lokasi</h2>
+      <p class="mt-4 text-md lg:text-lg md:text-lg">
+        Jl. Ahmad yani, RT01/RW05, Desa Mojosari Kecamatan Cugenang, Kabupaten
+        Indramayu
+      </p>
+    </div>
+    <div class="mt-4 comment">
+      <h3 class="text-2xl font-bold lg:text-3xl md:text-3xl">
+        Belum ada tanggapan
+      </h3>
+    </div>
+    <div class="mt-6 status">
+      <span
+        class="text-white bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-md px-8 py-2.5 mr-2 mb-2 dark:bg-blue-600 focus:outline-none dark:focus:ring-blue-800 text-base lg:text-lg md:text-lg"
+        >Process</span
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
