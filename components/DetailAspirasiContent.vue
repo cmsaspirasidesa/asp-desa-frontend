eb7862a020922c59eb5314ec50c1b0e8477e006d
rhenoresky <rheno2002@gmail.com>
2023-07-24 11:16:31 +0700

    refactor: add & update detail aspirasi

--- components/DetailAspirasiContent.vue
+++ components/DetailAspirasiContent.vue
@@ -1,7 +1,6 @@
 <template>
   <div
-    class="flex flex-col w-full h-screen px-5 pt-10 mx-auto bg-gray-200 xl:px-24 xl:pt-24 lg:pt-16 lg:px-16 md:py-16 md:px-16 md:mt-0 xl:min-h-screen"
-  >
+    class="flex flex-col w-full px-5 pt-10 mx-auto bg-gray-200 xl:px-24 xl:pt-24 lg:pt-16 lg:px-16 md:py-16 md:px-16 md:mt-0 pb-16">
     <div class="subject">
       <h1 class="text-3xl font-bold lg:text-4xl md:text-4xl">
         Pembangunan Lapangan
@@ -20,21 +19,19 @@
     </div>
     <div class="mt-4 location">
       <h2 class="text-2xl font-bold lg:text-3xl md:text-3xl">Lokasi</h2>
-      <p class="mt-4 text-md lg:text-lg md:text-lg">
+      <p class="mt-2 text-md lg:text-lg md:text-lg">
         Jl. Ahmad yani, RT01/RW05, Desa Mojosari Kecamatan Cugenang, Kabupaten
         Indramayu
       </p>
     </div>
-    <div class="mt-4 comment">
+    <div class="mt-6 comment">
       <h3 class="text-2xl font-bold lg:text-3xl md:text-3xl">
         Belum ada tanggapan
       </h3>
     </div>
-    <div class="mt-6 status">
+    <div class="mt-4 status">
       <span
-        class="text-white bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-md px-8 py-2.5 mr-2 mb-2 dark:bg-blue-600 focus:outline-none dark:focus:ring-blue-800 text-base lg:text-lg md:text-lg"
-        >Process</span
-      >
+        class="text-yellow-300 focus:ring-4 focus:ring-blue-300 font-semibold rounded-md mr-2 mb-2 text-2xl md:text-3xl">Process</span>
     </div>
   </div>
 </template>
