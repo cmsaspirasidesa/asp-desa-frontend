4a921bc1cef89788208c39b80bf3aa08dd37c3b8
dandnirv7 <dandnirv7@gmail.com>
2023-07-24 15:09:48 +0700

    refactor: change path import

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -1,44 +1,52 @@
 <script setup>
-import Masjid from '../assets/image/Masjid1.jpg'
+import Masjid from "../../assets/image/Masjid1.jpg";
 const aspirasi = [
   {
     id: 1,
-    judul: 'Bangun Masjid',
+    judul: "Bangun Masjid",
     imageSrc: Masjid,
-    status: 'Proses',
-    deskripsi: 'lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe',
+    status: "Proses",
+    deskripsi:
+      "lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe",
   },
   {
     id: 1,
-    judul: 'Bangun Masjid',
+    judul: "Bangun Masjid",
     imageSrc: Masjid,
-    status: 'Proses',
-    deskripsi: 'lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe',
+    status: "Proses",
+    deskripsi:
+      "lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe",
   },
   {
     id: 1,
-    judul: 'Bangun Masjid',
+    judul: "Bangun Masjid",
     imageSrc: Masjid,
-    status: 'Proses',
-    deskripsi: 'lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe',
+    status: "Proses",
+    deskripsi:
+      "lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe",
   },
   {
     id: 1,
-    judul: 'Bangun Masjid',
+    judul: "Bangun Masjid",
     imageSrc: Masjid,
-    status: 'Proses',
-    deskripsi: 'lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe',
+    status: "Proses",
+    deskripsi:
+      "lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe",
   },
-]
+];
 </script>
 
 <template>
-  <div class="flex flex-col md:flex-row gap-8 lg:mx-8">
+  <div class="flex flex-col gap-8 md:flex-row lg:mx-8">
     <FormAspirasi />
     <div>
       <FormSeacrh />
-      <div class="flex gap-y-6 mt-6 flex-wrap">
-        <CardAspirasi v-for="item of aspirasi" :aspirasi="item" :key="item.id" />
+      <div class="flex flex-wrap mt-6 gap-y-6">
+        <CardAspirasi
+          v-for="item of aspirasi"
+          :aspirasi="item"
+          :key="item.id"
+        />
       </div>
     </div>
   </div>
