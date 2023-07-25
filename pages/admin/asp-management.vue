aba24ca926a012a9daaf9b027fd19e4ba97ca1e3
Andika Eka Putra <andika.eka.102@gmail.com>
2023-07-25 09:31:18 +0700

    refactor: add title

--- pages/admin/asp-management.vue
+++ pages/admin/asp-management.vue
@@ -5,7 +5,8 @@ definePageMeta({
 </script>
 
 <template>
-  <main class="p-4 md:ml-64 h-auto pt-20">
+  <main class="flex flex-col p-4 md:ml-64 h-auto">
+    <div><h1 class="text-2xl mb-8 font-bold">Manajemen Aspirasi</h1></div>
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
       <div class="flex items-center justify-between pb-4">
         <div>
@@ -195,42 +196,49 @@ definePageMeta({
               scope="col"
               class="p-4"
             >
-              <div class="flex items-center">
-                <input
-                  id="checkbox-all-search"
-                  type="checkbox"
-                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                />
-                <label
-                  for="checkbox-all-search"
-                  class="sr-only"
-                  >checkbox</label
-                >
-              </div>
+              <span class="text-lg">#</span>
             </th>
             <th
               scope="col"
               class="px-6 py-3"
             >
-              Product name
+              Nama User
             </th>
             <th
               scope="col"
               class="px-6 py-3"
             >
-              Color
+              Judul
             </th>
             <th
               scope="col"
               class="px-6 py-3"
             >
-              Category
+              Alamat
             </th>
             <th
               scope="col"
               class="px-6 py-3"
             >
-              Price
+              Deskripsi
+            </th>
+            <th
+              scope="col"
+              class="px-6 py-3"
+            >
+              Lokasi
+            </th>
+            <th
+              scope="col"
+              class="px-6 py-3"
+            >
+              Komentar
+            </th>
+            <th
+              scope="col"
+              class="px-6 py-3"
+            >
+              Status
             </th>
             <th
               scope="col"
@@ -245,208 +253,126 @@ definePageMeta({
             class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
           >
             <td class="w-4 p-4">
-              <div class="flex items-center">
-                <input
-                  id="checkbox-table-search-1"
-                  type="checkbox"
-                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                />
-                <label
-                  for="checkbox-table-search-1"
-                  class="sr-only"
-                  >checkbox</label
-                >
-              </div>
+              <span class="text-black font-semibold">1</span>
             </td>
             <th
               scope="row"
               class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
             >
-              Apple MacBook Pro 17"
+            
             </th>
-            <td class="px-6 py-4">Silver</td>
-            <td class="px-6 py-4">Laptop</td>
-            <td class="px-6 py-4">$2999</td>
-            <td class="px-6 py-4">
-              <a
-                href="#"
-                class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >Edit</a
-              >
-            </td>
+            <td class="px-6 py-4"></td>
+            <td class="px-6 py-4"></td>
+            <td class="px-6 py-4"></td>
+            <td class="px-6 py-4"></td>
           </tr>
           <tr
             class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
           >
             <td class="w-4 p-4">
-              <div class="flex items-center">
-                <input
-                  id="checkbox-table-search-2"
-                  type="checkbox"
-                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                />
-                <label
-                  for="checkbox-table-search-2"
-                  class="sr-only"
-                  >checkbox</label
-                >
-              </div>
+              <span class="text-black font-semibold">2</span>
             </td>
             <th
               scope="row"
               class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
             >
-              Microsoft Surface Pro
             </th>
-            <td class="px-6 py-4">White</td>
-            <td class="px-6 py-4">Laptop PC</td>
-            <td class="px-6 py-4">$1999</td>
-            <td class="px-6 py-4">
-              <a
-                href="#"
-                class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >Edit</a
-              >
-            </td>
+            <td class="px-6 py-4"></td>
+            <td class="px-6 py-4"></td>
+            <td class="px-6 py-4"></td>
+            <td class="px-6 py-4"></td>
+            
           </tr>
           <tr
             class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
           >
             <td class="w-4 p-4">
-              <div class="flex items-center">
-                <input
-                  id="checkbox-table-search-3"
-                  type="checkbox"
-                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                />
-                <label
-                  for="checkbox-table-search-3"
-                  class="sr-only"
-                  >checkbox</label
-                >
-              </div>
+              <span class="text-black font-semibold">3</span>
             </td>
             <th
               scope="row"
               class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
             >
-              Magic Mouse 2
+
             </th>
-            <td class="px-6 py-4">Black</td>
-            <td class="px-6 py-4">Accessories</td>
-            <td class="px-6 py-4">$99</td>
-            <td class="px-6 py-4">
-              <a
-                href="#"
-                class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >Edit</a
-              >
-            </td>
-          </tr>
+            <td class="px-6 py-4"></td>
+            <td class="px-6 py-4"></td>
+            <td class="px-6 py-4"></td>
+            <td class="px-6 py-4"></td>
+            
+          </tr>      
           <tr
             class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
           >
             <td class="w-4 p-4">
-              <div class="flex items-center">
-                <input
-                  id="checkbox-table-3"
-                  type="checkbox"
-                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                />
-                <label
-                  for="checkbox-table-3"
-                  class="sr-only"
-                  >checkbox</label
-                >
-              </div>
+              <span class="text-black font-semibold">4</span>
             </td>
             <th
               scope="row"
               class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
             >
-              Apple Watch
+              
             </th>
-            <td class="px-6 py-4">Silver</td>
-            <td class="px-6 py-4">Accessories</td>
-            <td class="px-6 py-4">$179</td>
-            <td class="px-6 py-4">
-              <a
-                href="#"
-                class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >Edit</a
-              >
-            </td>
-          </tr>
+            <td class="px-6 py-4"></td>
+            <td class="px-6 py-4"></td>
+            <td class="px-6 py-4"></td>
+            <td class="px-6 py-4"></td>
+            
+          </tr>      
           <tr
             class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
           >
             <td class="w-4 p-4">
-              <div class="flex items-center">
-                <input
-                  id="checkbox-table-3"
-                  type="checkbox"
-                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                />
-                <label
-                  for="checkbox-table-3"
-                  class="sr-only"
-                  >checkbox</label
-                >
-              </div>
+              <span class="text-black font-semibold">5</span>
             </td>
             <th
               scope="row"
               class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
             >
-              iPad
+              
             </th>
-            <td class="px-6 py-4">Gold</td>
-            <td class="px-6 py-4">Tablet</td>
-            <td class="px-6 py-4">$699</td>
-            <td class="px-6 py-4">
-              <a
-                href="#"
-                class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >Edit</a
-              >
-            </td>
-          </tr>
+            <td class="px-6 py-4"></td>
+            <td class="px-6 py-4"></td>
+            <td class="px-6 py-4"></td>
+            <td class="px-6 py-4"></td>
+            
+          </tr>      
           <tr
-            class="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600"
+            class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
           >
             <td class="w-4 p-4">
-              <div class="flex items-center">
-                <input
-                  id="checkbox-table-3"
-                  type="checkbox"
-                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                />
-                <label
-                  for="checkbox-table-3"
-                  class="sr-only"
-                  >checkbox</label
-                >
-              </div>
+              <span class="text-black font-semibold">6</span>
             </td>
             <th
               scope="row"
               class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
             >
-              Apple iMac 27"
+              
             </th>
-            <td class="px-6 py-4">Silver</td>
-            <td class="px-6 py-4">PC Desktop</td>
-            <td class="px-6 py-4">$3999</td>
-            <td class="px-6 py-4">
-              <a
-                href="#"
-                class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >Edit</a
-              >
-            </td>
-          </tr>
+            <td class="px-6 py-4"></td>
+            <td class="px-6 py-4"></td>
+            <td class="px-6 py-4"></td>
+            <td class="px-6 py-4"></td>
+            
+          </tr>      
+          
+          
+          
         </tbody>
       </table>
     </div>
+    <nav
+      class="flex items-center justify-between pt-4"
+      aria-label="Table navigation"
+    >
+      <span class="text-sm font-normal text-gray-500 dark:text-gray-400"
+        >Showing
+        <span class="font-semibold text-gray-900 dark:text-white">1-10</span> of
+        <span class="font-semibold text-gray-900 dark:text-white"
+          >1000</span
+        ></span
+      >
+      <PaginationBlock />
+    </nav>
   </main>
 </template>
