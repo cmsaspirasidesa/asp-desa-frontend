17f0abed9b5ea62b0b2fdf5797edd7d28d5ecd83
Andika Eka Putra <andika.eka.102@gmail.com>
2023-07-25 13:44:47 +0700

    refactor: add user table to admin dashboard

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -5,12 +5,35 @@ definePageMeta({
 </script>
 
 <template>
-  <main class="flex flex-col p-4 md:ml-64 h-auto">
+  <main class="flex flex-col gap-5 p-4 md:ml-64 h-auto">
     <div class="flex justify-center">
       <ClientOnly>
         <BarChart />
         <LineChart />
       </ClientOnly>
+    </div>
+    <div class="flex">
+      <div class="flex flex-col flex-1">
+        <div>
+          <h1 class="text-xl">Majemen User</h1>
+        </div>
+        <div>
+          <UserTable />
+        </div>
+        <div>
+          <PaginationBlock />
+        </div>
+      </div>
+    </div>
+    <div class="flex">
+      <div class="flex flex-col flex-1">
+        <div>
+          <UserTable />
+        </div>
+        <div>
+          <PaginationBlock />
+        </div>
+      </div>
     </div>
   </main>
 </template>
