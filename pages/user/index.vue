4e32cc64dc4321e781decb8ce21148de8ca1a98b
dandnirv7 <dandnirv7@gmail.com>
2023-07-26 14:04:59 +0700

    refactor: modify user pages and FormAspirasi components

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -1,6 +1,6 @@
 <script setup>
-import Masjid from '../../assets/image/Masjid1.jpg'
-import Masjid2 from '../../assets/image/Masjid2.jpg'
+import Masjid from "../../assets/image/Masjid1.jpg";
+import Masjid2 from "../../assets/image/Masjid2.jpg";
 const aspirasi = [
   {
     id: 1,
@@ -34,20 +34,26 @@ const aspirasi = [
     deskripsi:
       "lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe",
   },
-]
+];
 
 definePageMeta({
-  layout: 'user'
-})
+  layout: "user",
+});
 </script>
 
 <template>
-  <div class="flex flex-col max-w-screen-xl gap-8 md:flex-row lg:mx-8">
-    <FormAspirasi />
+  <div
+    class="flex flex-col max-w-screen-xl lg:flex-col xl:flex-row gap-8 md:flex-col lg:mx-12 xl:mx-16 mt-5"
+  >
+    <FormAspirasiUser />
     <div>
       <FormSeacrh />
-      <NuxtLink to="/detail_aspirasi" class="flex flex-wrap my-6 gap-y-6">
-        <CardAspirasi v-for="item of aspirasi" :aspirasi="item" :key="item.id" />
+      <NuxtLink to="/detail-aspirasi" class="flex flex-wrap my-6 gap-y-6">
+        <CardAspirasi
+          v-for="item of aspirasi"
+          :aspirasi="item"
+          :key="item.id"
+        />
       </NuxtLink>
     </div>
   </div>
