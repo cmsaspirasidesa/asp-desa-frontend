f81c75348536d5479e65a5a8305d20b88a719a44
rhenoresky <rheno2002@gmail.com>
2023-07-26 20:37:02 +0700

    refactor: change name variable

--- components/UserNavbar.vue
+++ components/UserNavbar.vue
@@ -1,4 +1,5 @@
 <script setup>
+const {data, signOut} = useAuth()
 import desa from '../assets/image/Desa.png'
 </script>
 
@@ -7,77 +8,22 @@ import desa from '../assets/image/Desa.png'
     <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
       <div class="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto">
         <div class="flex items-center justify-start">
-          <button data-drawer-target="drawer-navigation" data-drawer-toggle="drawer-navigation"
-            aria-controls="drawer-navigation"
-            class="p-2 mr-2 text-gray-600 rounded-lg cursor-pointer md:hidden hover:text-gray-900 hover:bg-gray-100 focus:bg-gray-100 dark:focus:bg-gray-700 focus:ring-2 focus:ring-gray-100 dark:focus:ring-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-            <svg aria-hidden="true" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd"
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clip-rule="evenodd"></path>
-            </svg>
-            <svg aria-hidden="true" class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clip-rule="evenodd"></path>
-            </svg>
-            <span class="sr-only">Toggle sidebar</span>
-          </button>
-          <a href="https://flowbite.com" class="flex items-center justify-between mr-4">
+          <a class="flex items-center justify-between mr-4">
             <img :src="desa" class="h-8 mr-3" alt="Flowbite Logo" />
             <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Aspirasi Desa</span>
           </a>
         </div>
         <div class="flex items-center lg:order-2">
-          <button type="button" data-drawer-toggle="drawer-navigation" aria-controls="drawer-navigation"
-            class="p-2 mr-1 text-gray-500 rounded-lg md:hidden hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600">
-            <span class="sr-only">Toggle search</span>
-            <svg aria-hidden="true" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg">
-              <path clip-rule="evenodd" fill-rule="evenodd"
-                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z">
-              </path>
-            </svg>
-          </button>
-          <!-- Notifications -->
-
           <!-- Dropdown menu -->
           <div
             class="z-50 hidden max-w-sm my-4 overflow-hidden text-base list-none bg-white divide-y divide-gray-100 shadow-lg dark:divide-gray-600 dark:bg-gray-700 rounded-xl"
             id="notification-dropdown">
-            <div
-              class="block px-4 py-2 text-base font-medium text-center text-gray-700 bg-gray-50 dark:bg-gray-600 dark:text-gray-300">
-              Notifications
-            </div>
             <div>
               <a href="#" class="flex px-4 py-3 border-b hover:bg-gray-100 dark:hover:bg-gray-600 dark:border-gray-600">
                 <div class="flex-shrink-0">
                   <img class="rounded-full w-11 h-11"
                     src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png"
                     alt="Bonnie Green avatar" />
-                  <div
-                    class="absolute flex items-center justify-center w-5 h-5 ml-6 -mt-5 border border-white rounded-full bg-primary-700 dark:border-gray-700">
-                    <svg aria-hidden="true" class="w-3 h-3 text-white" fill="currentColor" viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg">
-                      <path
-                        d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z">
-                      </path>
-                      <path
-                        d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z">
-                      </path>
-                    </svg>
-                  </div>
-                </div>
-                <div class="w-full pl-3">
-                  <div class="text-gray-500 font-normal text-sm mb-1.5 dark:text-gray-400">
-                    New message from
-                    <span class="font-semibold text-gray-900 dark:text-white">Bonnie Green</span>: "Hey, what's up? All
-                    set for the presentation?"
-                  </div>
-                  <div class="text-xs font-medium text-primary-600 dark:text-primary-500">
-                    a few moments ago
-                  </div>
                 </div>
               </a>
               <a href="#" class="flex px-4 py-3 border-b hover:bg-gray-100 dark:hover:bg-gray-600 dark:border-gray-600">
@@ -324,8 +270,8 @@ import desa from '../assets/image/Desa.png'
             class="z-50 hidden w-56 my-4 text-base list-none bg-white divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600 rounded-xl"
             id="dropdown">
             <div class="px-4 py-3">
-              <span class="block text-sm font-semibold text-gray-900 dark:text-white">Neil Sims</span>
-              <span class="block text-sm text-gray-900 truncate dark:text-white">name@flowbite.com</span>
+              <span class="block text-sm font-semibold text-gray-900 dark:text-white">{{ data.user.name }}</span>
+              <span class="block text-sm text-gray-900 truncate dark:text-white">{{ data.user.email }}</span>
             </div>
             <ul class="py-1 text-gray-700 dark:text-gray-300" aria-labelledby="dropdown">
               <li>
@@ -334,13 +280,10 @@ import desa from '../assets/image/Desa.png'
               </li>
             </ul>
             <ul class="py-1 text-gray-700 dark:text-gray-300" aria-labelledby="dropdown">
-              <li>
-                <NuxtLink
-                  to="#"
-                  class="block px-4 py-2 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-                  Keluar
-                  <Icon name="ic:outline-logout"/>
-                </NuxtLink>
+              <li @click="signOut()"
+                class="block px-4 py-2 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                Keluar
+                <Icon name="ic:outline-logout" />
               </li>
             </ul>
           </div>
