5a94096fa069d0a703cffcb1a71e58ddae56b877
dandnirv7 <dandnirv7@gmail.com>
2023-07-26 14:44:45 +0700

    refactor: rename variable

--- components/CardAspirasi.vue
+++ components/CardAspirasi.vue
@@ -1,23 +1,34 @@
 <template>
-  <div class="max-w-[350px] p-6 mx-auto border border-solid border-gray-300 rounded-[12px]">
-    <div :key="aspirasi.id" class="group relative">
+  <div
+    class="max-w-[350px] p-6 mx-auto border border-solid border-gray-300 rounded-[12px]"
+  >
+    <div :key="aspirations.id" class="group relative">
       <div class="h-[250px] overflow-hidden rounded-md group-hover:opacity-75">
-        <img :src="aspirasi.imageSrc" alt="Gambar Aspirasi" class="h-full w-full object-cover object-center" />
+        <img
+          :src="aspirations.imageSrc"
+          alt="Gambar Aspirasi"
+          class="h-full w-full object-cover object-center"
+        />
       </div>
       <div class="mt-4 flex justify-between">
         <div>
           <h3 class="text-lg font-medium text-gray-700">
-            {{ aspirasi.judul }}
+            {{ aspirations.headline }}
           </h3>
         </div>
-        <p class="text-sm font-medium border border-solid border-gray-300 rounded-md px-2 py-1 text-gray-900">{{
-          aspirasi.status }}</p>
+        <p
+          class="text-sm font-medium border border-solid border-gray-300 rounded-md px-2 py-1 text-gray-900"
+        >
+          {{ aspirations.status }}
+        </p>
       </div>
       <p class="mt-3 text-justify text-sm line-clamp-4 text-gray-500">
-        {{ aspirasi.deskripsi }}
+        {{ aspirations.description }}
       </p>
-      <div class=" mt-5">
-        <a href="#" class="px-4 py-2 bg-indigo-600 rounded-md text-white">Detail</a>
+      <div class="mt-5">
+        <a href="#" class="px-4 py-2 bg-indigo-600 rounded-md text-white"
+          >Detail</a
+        >
       </div>
     </div>
   </div>
@@ -25,8 +36,8 @@
 
 <script setup>
 defineProps({
-  aspirasi: {
+  aspirations: {
     type: Object,
-  }
-})
+  },
+});
 </script>
