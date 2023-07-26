dcdea18d8afbc01a0922c613d5f99f7ffcfb831a
Andika Eka Putra <andika.eka.102@gmail.com>
2023-07-26 18:58:34 +0700

    refactor: update aspiration form

--- components/FormAspirasi.vue
+++ components/FormAspirasi.vue
@@ -1,135 +1,246 @@
 <script setup>
-const gambar = reactive([])
-const notImage = ref(false)
-const batas = ref(false)
+const images = reactive([]);
+const notImage = ref(false);
+const batas = ref(false);
 
 function createImage(files) {
-  batas.value = false
-  notImage.value = false
-  if (files[0].type.split("/")[0] === 'image') {
+  batas.value = false;
+  notImage.value = false;
+  if (files[0].type.split('/')[0] === 'image') {
     for (let index = 0; index < files.length; index++) {
       let reader = new FileReader();
       reader.onload = function (event) {
         const imageUrl = event.target.result;
-        if (gambar.length >= 4) {
-          notImage.value = false
-          batas.value = true
+        if (images.length >= 4) {
+          notImage.value = false;
+          batas.value = true;
         } else {
-          gambar.push(imageUrl);
+          images.push(imageUrl);
         }
-      }
+      };
       reader.readAsDataURL(files[index]);
     }
   } else {
-    batas.value = false
-    notImage.value = true
+    batas.value = false;
+    notImage.value = true;
   }
 }
 
 function onOver(e) {
   e.preventDefault();
-  e.dataTransfer.dropEffect = "copy";
+  e.dataTransfer.dropEffect = 'copy';
 }
 
 function onDrop(e) {
   e.preventDefault();
   const files = e.dataTransfer.files;
-  createImage(files)
+  createImage(files);
 }
 
 function onFileChange(e) {
-  var files = e.target.files
+  var files = e.target.files;
   createImage(files);
 }
 
 function removeImage(index) {
-  gambar.splice(index, 1)
+  images.splice(index, 1);
 }
 
+const formAddData = ref({
+  title: '',
+  description: '',
+  aspLocation: '',
+  formImages: images
+})
+
+const emit = defineEmits(['userAddsAsp'])
 </script>
 
 <template>
-  <form
-    class="bg-white md:max-w-[400px] max-w-[350px] w-full m-auto flex flex-col px-6 py-3 rounded-[12px] md:border md:border-solid md:border-gray-300">
-    <div>
-      <div class="px-4 mx-auto">
-        <h2 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white text-center">Form Aspirasi</h2>
-      </div>
-      <label for="nama" class="block text-sm font-medium leading-6 text-gray-900">Nama</label>
-      <div class="mt-2">
-        <input type="text" required="true" name="nama" id="nama" autocomplete="nama"
-          class="block w-full bg-gray-50 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-      </div>
-    </div>
-    <div class="mt-2">
-      <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email</label>
-      <div class="mt-2">
-        <input type="email" required="true" name="email" id="email" autocomplete="email"
-          class="block w-full bg-gray-50 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-      </div>
-    </div>
-    <div class="mt-2">
-      <label for="judul" required="true" class="block text-sm font-medium leading-6 text-gray-900">Judul</label>
-      <div class="mt-2">
-        <input type="text" name="judul" id="judul" autocomplete="judul"
-          class="block w-full bg-gray-50 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-      </div>
-    </div>
-    <div class="mt-2">
-      <label for="alamat" required="true" class="block text-sm font-medium leading-6 text-gray-900">Alamat</label>
-      <div class="mt-2">
-        <input type="text" name="alamat" id="alamat" autocomplete="alamat"
-          class="block w-full bg-gray-50 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-      </div>
-    </div>
-    <div class="mt-2">
-      <label for="deskripsi" class="block text-sm font-medium leading-6 text-gray-900">Deskripsi</label>
-      <div class="mt-2">
-        <textarea id="deskripsi" name="deskripsi" rows="5"
-          class="block w-full bg-gray-50 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-      </div>
-    </div>
-
-    <div class="mt-2">
-      <div class="flex justify-between">
-        <p class="text-sm font-medium leading-6 text-gray-900">Foto</p>
-        <div v-if="notImage">
-          <p class="text-red-600 leading-6 text-sm font-medium">File harus image</p>
+  <div>
+    <form
+      @submit.prevent
+      class="bg-white md:max-w-[400px] max-w-[350px] w-full m-auto flex flex-col px-6 py-3 rounded-[12px] md:border md:border-solid md:border-gray-300"
+    >
+      <div class="hidden">
+        <div class="px-4 mx-auto">
+          <h2
+            class="mb-2 text-3xl font-bold text-gray-900 dark:text-white text-center"
+          >
+            Form Aspirasi
+          </h2>
         </div>
-        <div v-if="batas">
-          <p class="text-red-600 leading-6 text-sm font-medium">Maksimal 4 image</p>
+        <label
+          for="nama"
+          class="block text-sm font-medium leading-6 text-gray-900"
+          >Nama</label
+        >
+        <div class="mt-2">
+          <input
+            type="text"
+            required="true"
+            name="nama"
+            id="nama"
+            autocomplete="nama"
+            class="block w-full bg-gray-50 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
         </div>
       </div>
-      <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10" @drop="onDrop"
-        @dragover="onOver">
-        <div class="text-center">
-          <Icon name="heroicons-solid:photo" class="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
-          <div class="mt-4 flex text-sm leading-6 text-gray-600">
-            <label for="foto"
-              class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
-              <span>Upload a file</span>
-              <input id="foto" name="foto" multiple type="file" class="sr-only" @change="onFileChange" accept="image/*" />
-            </label>
-            <p class="pl-1">or drag and drop</p>
+      <div class="mt-2 hidden">
+        <label
+          for="email"
+          class="block text-sm font-medium leading-6 text-gray-900"
+          >Email</label
+        >
+        <div class="mt-2">
+          <input
+            type="email"
+            required="true"
+            name="email"
+            id="email"
+            autocomplete="email"
+            class="block w-full bg-gray-50 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+        </div>
+      </div>
+      <div class="mt-2">
+        <label
+          for="judul"
+          required="true"
+          class="block text-sm font-medium leading-6 text-gray-900"
+          >Judul</label
+        >
+        <div class="mt-2">
+          <input
+            v-model="formAddData.title"
+            type="text"
+            name="judul"
+            id="judul"
+            autocomplete="judul"
+            class="block w-full bg-gray-50 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+        </div>
+      </div>
+      <div class="mt-2">
+        <label
+          for="alamat"
+          required="true"
+          class="block text-sm font-medium leading-6 text-gray-900"
+          >Alamat</label
+        >
+        <div class="mt-2">
+          <input
+            v-model="formAddData.aspLocation"
+            type="text"
+            name="alamat"
+            id="alamat"
+            autocomplete="alamat"
+            class="block w-full bg-gray-50 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+        </div>
+      </div>
+      <div class="mt-2">
+        <label
+          for="deskripsi"
+          class="block text-sm font-medium leading-6 text-gray-900"
+          >Deskripsi</label
+        >
+        <div class="mt-2">
+          <textarea
+            v-model="formAddData.description"
+            id="deskripsi"
+            name="deskripsi"
+            rows="5"
+            class="block w-full bg-gray-50 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+        </div>
+      </div>
+      <div class="mt-2">
+        <div class="flex justify-between">
+          <p class="text-sm font-medium leading-6 text-gray-900">Foto</p>
+          <div v-if="notImage">
+            <p class="text-red-600 leading-6 text-sm font-medium">
+              File harus gambar
+            </p>
           </div>
-          <p class="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
+          <div v-if="batas">
+            <p class="text-red-600 leading-6 text-sm font-medium">
+              Maksimal 4 gambar
+            </p>
+          </div>
+        </div>
+        <div
+          class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10"
+          @drop="onDrop"
+          @dragover="onOver"
+        >
+          <div class="text-center">
+            <Icon
+              name="heroicons-solid:photo"
+              class="mx-auto h-12 w-12 text-gray-300"
+              aria-hidden="true"
+            />
+            <div class="mt-4 flex text-sm leading-6 text-gray-600">
+              <label
+                for="foto"
+                class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+              >
+                <span>Upload a file</span>
+                <input
+                  id="foto"
+                  name="foto"
+                  multiple
+                  type="file"
+                  class="sr-only"
+                  @change="onFileChange"
+                  accept="image/*"
+                />
+              </label>
+              <p class="pl-1">or drag and drop</p>
+            </div>
+            <p class="text-xs leading-5 text-gray-600">
+              PNG, JPG, GIF up to 10MB
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-
-    <div v-if="gambar" class="flex flex-wrap w-full gap-2 mt-2 justify-center">
-      <div v-for="(foto, index) in gambar" class="bg-orange-400 rounded-md overflow-hidden w-[45%] h-[100px] relative">
-        <img :src="foto" alt="Gambar Aspirasi" class="h-full w-full object-cover object-center" />
-        <button type="button" @click="removeImage(index)"
-          class="absolute right-1 top-1 w-5 h-5 bg-red-600 flex justify-center items-center rounded-[50%]">
-          <Icon name="ci:close-md" class="text-white" />
+      <div
+        v-if="images"
+        class="flex flex-wrap w-full gap-2 mt-2 justify-center"
+      >
+        <div
+          v-for="(foto, index) in images"
+          class="bg-orange-400 rounded-md overflow-hidden w-[45%] h-[100px] relative"
+        >
+          <img
+            :src="foto"
+            alt="Gambar Aspirasi"
+            class="h-full w-full object-cover object-center"
+          />
+          <button
+            type="button"
+            @click="removeImage(index)"
+            class="absolute right-1 top-1 w-5 h-5 bg-red-600 flex justify-center items-center rounded-[50%]"
+          >
+            <Icon
+              name="ci:close-md"
+              class="text-white"
+            />
+          </button>
+        </div>
+      </div>
+      <div class="mt-2 flex items-center justify-end gap-x-6">
+        <button
+          @="
+            status === 'authenticated' && data.role === 'USER'
+              ? { click: () => $emit('userAddsAsp', formAddData) }
+              : { click: () => console.log('guest') }
+          "
+          class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Kirim
         </button>
       </div>
-    </div>
-
-    <div class="mt-2 flex items-center justify-end gap-x-6">
-      <button type="submit"
-        class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Kirim</button>
-    </div>
-  </form>
+    </form>
+  </div>
 </template>
--- pages/user/index.vue
+++ pages/user/index.vue
@@ -1,53 +1,78 @@
 <script setup>
-import Masjid from '../../assets/image/Masjid1.jpg'
-import Masjid2 from '../../assets/image/Masjid2.jpg'
+import Masjid from '../../assets/image/Masjid1.jpg';
+import Masjid2 from '../../assets/image/Masjid2.jpg';
 const aspirasi = [
   {
     id: 1,
-    judul: "Bangun Masjid",
+    judul: 'Bangun Masjid',
     imageSrc: Masjid,
-    status: "Proses",
+    status: 'Proses',
     deskripsi:
-      "lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe",
+      'lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe',
   },
   {
     id: 1,
-    judul: "Bangun Masjid",
+    judul: 'Bangun Masjid',
     imageSrc: Masjid2,
-    status: "Proses",
+    status: 'Proses',
     deskripsi:
-      "lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe",
+      'lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe',
   },
   {
     id: 1,
-    judul: "Bangun Masjid",
+    judul: 'Bangun Masjid',
     imageSrc: Masjid,
-    status: "Proses",
+    status: 'Proses',
     deskripsi:
-      "lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe",
+      'lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe',
   },
   {
     id: 1,
-    judul: "Bangun Masjid",
+    judul: 'Bangun Masjid',
     imageSrc: Masjid2,
-    status: "Proses",
+    status: 'Proses',
     deskripsi:
-      "lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe",
+      'lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe lorem ipsum dolor smit are nawa kombe',
   },
-]
+];
+
+const { data } = useAuth();
+
+const handleUserAddsAsp = async (formAddData) => {
+  await useFetch('http://localhost:6969/aspirations', {
+    method: 'POST',
+    headers: {
+      'authorization': data.token.jwt, 
+      'Content-Type': 'multipart/form-data',
+    },
+    body: {
+      judul: formAddData.title,
+      deskripsi: formAddData.description,
+      lokasi: formAddData.aspLocation,
+      images: formAddData.formImages
+    }
+  })
+}
 
 definePageMeta({
-  layout: 'user'
-})
+  layout: 'user',
+});
 </script>
 
 <template>
   <div class="flex flex-col max-w-screen-xl gap-8 md:flex-row lg:mx-8">
-    <FormAspirasi />
+    <FormAspirasi @user-adds-asp="handleUserAddsAsp" />
     <div>
       <FormSeacrh />
-      <NuxtLink to="/detail_aspirasi" class="flex flex-wrap my-6 gap-y-6">
-        <CardAspirasi v-for="item of aspirasi" :aspirasi="item" :key="item.id" />
+      <NuxtLink
+        to="/detail_aspirasi"
+        class="flex flex-wrap my-6 gap-y-6"
+      >
+        <CardAspirasi
+          v-for="item of aspirasi"
+          :aspirasi="item"
+          :key="item.id"
+        />
       </NuxtLink>
     </div>
   </div>
