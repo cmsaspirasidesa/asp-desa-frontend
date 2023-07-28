6fc25556b5ad6e26d8331adc5339c1b73c67ade7
Andika Eka Putra <andika.eka.102@gmail.com>
2023-07-28 15:08:27 +0700

    feature: delete user in admin (not yet auto reload)

--- components/UserTable.vue
+++ components/UserTable.vue
@@ -3,10 +3,11 @@ const userData = defineProps({
   users: Object,
 });
 
-const isShowModal = ref(false);
+const isShowUpdateModal = ref(false);
+const isShowDeleteModal = ref(false);
 
 function showModal(user) {
-  isShowModal.value = true;
+  isShowUpdateModal.value = true;
   formData.value.userId = user.id;
   formData.value.nik = user.nik;
   formData.value.name = user.nama;
@@ -15,7 +16,16 @@ function showModal(user) {
 }
 
 function closeModal() {
-  isShowModal.value = false;
+  isShowUpdateModal.value = false;
+}
+
+function showDeleteModal(userId) {
+  isShowDeleteModal.value = true
+  formData.value.userId = userId
+}
+
+function closeDeleteModal() {
+  isShowDeleteModal.value = false
 }
 
 const formData = ref({
@@ -44,15 +54,32 @@ const updateUser = async () => {
       },
     });
 
-    isShowModal.value = false
-    console.log('email: ', formData.value);
-    console.log('alamat: ', formData.value);
+    isShowUpdateModal.value = false
     console.log('server response: ', response);
   } catch (e) {
-    isShowModal.value = false
+    isShowUpdateModal.value = false
     console.log(e);
   }
 };
+
+const deleteUser = async () => {
+  try {
+    const response = await useFetch(`http://localhost:6969/users/${formData.value.userId}`, {
+      method: 'DELETE',
+      headers: {
+      'Authorization': accessToken.jwt
+      }
+    });
+
+    isShowDeleteModal.value = false
+    console.log('server response: ', response);
+  } catch (e) {
+    isShowUpdateModal.value = false
+    console.log(e);
+  }
+};
+
+
 </script>
 
 <template>
@@ -125,6 +152,8 @@ const updateUser = async () => {
               />
             </button>
             <button
+              @click="showDeleteModal(user.id)"
+              type="button"
               class="hover:bg-red-900 pb-1 px-1 rounded text-red-500 hover:text-red-200"
             >
               <Icon
@@ -137,7 +166,7 @@ const updateUser = async () => {
       </tbody>
     </table>
     <div
-      v-if="isShowModal"
+      v-if="isShowUpdateModal"
       class="fixed z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full"
     >
       <div
@@ -277,5 +306,46 @@ const updateUser = async () => {
       </div>
     </div>
     
+    <div
+    v-if="isShowDeleteModal"
+    @click="showDeleteModal"
+    class="fixed top-0 left-0 right-0 z-50 flex items-center justify-center w-full overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full"
+  >
+    <div class="bg-black opacity-50 fixed top-0 left-0 right-0 z-50 items-center justify-center w-full overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full"></div>
+    <div class="relative z-50 w-full h-full max-w-md p-4 md:h-auto">
+      <!-- Modal content -->
+      <div
+        class="relative p-4 text-center bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5"
+      >
+        <button
+          type="button"
+          class="text-gray-400 absolute top-2.5 right-2.5 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+          data-modal-toggle="deleteModal"
+        >
+          <Icon name="heroicons-solid:x" size="1rem" />
+          <span class="sr-only">Close modal</span>
+        </button>
+        <p class="mb-4 text-gray-500 dark:text-gray-300">
+          Apakah anda yakin untuk menghapus user?
+        </p>
+        <div class="flex items-center justify-center space-x-4">
+          <button
+            @click="deleteUser"
+            type="submit"
+            class="px-3 py-2 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900"
+          >
+            Ya, Hapus
+          </button>
+          <button
+            @click="closeDeleteModal"
+            type="button"
+            class="px-3 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+          >
+            Batal
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
   </div>
 </template>
